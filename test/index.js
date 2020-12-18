fce7c3805cc6cf131f80bbdb6cd39704538b1871
B <benricheson101@gmail.com>
2020-12-18 09:40:33 -0500

    fixed test/index.js

--- test/index.js
+++ test/index.js
@@ -1,18 +1,8 @@
-// const {Statuspage} = require('../build/src');
-// const assert = require('assert');
+const {Statuspage} = require('../build/src');
+const assert = require('assert');
 
-// const st = new Statuspage('wgbgn12kd4gt');
+const st = new Statuspage('wgbgn12kd4gt');
 
-// st.summary().then(({status}) =>
-//   assert(status.description === 'All Systems Operational')
-// );
-
-const {StatuspageUpdates} = require('../build/src');
-
-const s = new StatuspageUpdates('wgbgn12kd4gt', 5000);
-
-s.on('INCIDENT_UPDATE', e =>
-  console.log('Updoot!', e.incidents[0].incident_updates[0].body)
+st.summary().then(({status}) =>
+  assert(status.description === 'All Systems Operational')
 );
-
-s.start();
